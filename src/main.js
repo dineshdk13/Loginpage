84896f0986c1bde4d389d2f0742e4d637bd6cd1a
Dinesh <dinesh.k131997@gmail.com>
2020-07-26 11:15:22 +0530

    Add

--- src/main.js
+++ src/main.js
@@ -1,10 +1,24 @@
-import Vue from "vue";
-import App from "./App.vue";
-import "./registerServiceWorker";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue'
+import App from './App.vue'
+import router from './router';
+import firebase from 'firebase';
+import store from './store'
 
 Vue.config.productionTip = false;
+
+var firebaseConfig = {
+  apiKey: "AIzaSyB7VMmPQU70MBUSwye8LIiKISzBAUDaxmU",
+  authDomain: "loginpage-a0b10.firebaseapp.com",
+  databaseURL: "https://loginpage-a0b10.firebaseio.com",
+  projectId: "loginpage-a0b10",
+  storageBucket: "loginpage-a0b10.appspot.com",
+  messagingSenderId: "36787860726",
+  appId: "1:36787860726:web:27d2779b4a9dc323bb68d7",
+  measurementId: "G-RJZG2R3DMH"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
 
 new Vue({
   router,
---
+++ src/views/Login.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="container">
+      <h1>Login</h1>
+      <br>
+      <br>
+    <div class="row justify-content-md-center">
+      <div class="col-md-3">
+        <form>
+          <div class="form-group">
+            <input
+              type="email"
+              class="form-control"
+              id="exampleInputEmail1"
+              aria-describedby="emailHelp"
+              placeholder="Email"
+            />
+          </div>
+          <div class="form-group">
+            <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password"/>
+          </div>
+          <button type="submit" class="btn btn-primary">Login</button>
+        </form>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style scoped>
+</style>
